8b92e362a93810923e281bb4b247077cdfc859ac
Anastasia Kondratieva <anastasia012@yandex.ru>
2017-10-23 01:36:40 +0300

    table done, problem with sort

--- hw2/table.js
+++ hw2/table.js
@@ -1,5 +1,5 @@
-d3.json("countries_2012.json", function(error, data)
-{   
+function drow_table(data)
+{
     var table = d3.select("body").append("table"), thead = table.append("thead").attr("class", "thead");
     tbody = table.append("tbody")
     table.append("caption").html("World Countries Ranking");
@@ -27,10 +27,10 @@ d3.json("countries_2012.json", function(error, data)
                }
            })
 
-    var format = d3.format(",0s");
-    console.log(format(1990000000))
+    var gdp_format = d3.format(".3s");
+    var life_exp_format = d3.format(".1f");
+    var population_format = d3.format(",");
 
-    
     var rows = tbody
         .selectAll("tr.row")
         .data(data)
@@ -43,11 +43,11 @@ d3.json("countries_2012.json", function(error, data)
             return d3.range(desired_columns.length)
                 .map(function(column, i) {
                     if (desired_columns[i] == "gdp") {
-                        return format(row[desired_columns[i]]);
+                        return gdp_format(row[desired_columns[i]]);
                     } else if (desired_columns[i] == "population") {
-                        return format(row[desired_columns[i]]);
+                        return population_format(row[desired_columns[i]]);
                     } else if (desired_columns[i] == "life_expectancy") {
-                        return format(row[desired_columns[i]]);
+                        return life_exp_format(row[desired_columns[i]]);
                     }
                     return row[desired_columns[i]];
                 });
@@ -55,4 +55,66 @@ d3.json("countries_2012.json", function(error, data)
         .enter()
         .append("td")
         .text(function(d) { return d; })
+}
+
+d3.json("countries_2012.json", function(error, data)
+{
+    var choices = [];
+    
+    d3.selectAll('input[type=checkbox]').on("change", update_all);
+    d3.selectAll('input[type=radio]').on("change", update_all);
+    d3.selectAll('input[type=range]').on("change", update_all);
+    
+    function update_all()
+    {
+        d3.select('thead').remove();
+        d3.select('tbody').remove();
+        d3.select('caption').remove();
+
+        choices = [];
+        d3.selectAll("input[type=checkbox]")
+            .each(function(d) {
+                 cb = d3.select(this);
+                 if(cb.property("checked")){
+                     choices.push(cb.property("value"));
+                 }});
+
+        if (choices.length == 0) {
+            choices = ['Americas', 'Africa', 'Asia', 'Europe', 'Oceania'];
+        }
+
+        var year = d3.select('input[type=range]').property('value');
+        
+        var new_data = data.filter(function(d,i) { return choices.includes(d['continent']) && d['year'] == year; })
+
+        r = d3.select("#aggregation");
+        if (r.property("checked")) {
+
+            new_data = d3.nest().key(function(d,i) { return d['continent']; })
+                .rollup(function(rows) {
+                    var aggr_row = {
+                        gdp: 0,
+                        continent: rows[0]['continent'],
+                        name: rows[0]['continent'],
+                        life_expectancy: 0,
+                        population: 0,
+                        year: rows[0]['year']
+                    };
+                    aggr_row['gdp'] = d3.sum(rows, function(d) { return d['gdp']; });
+                    aggr_row['life_expectancy'] = d3.mean(rows, function(d) { return d['life_expectancy']; });
+                    aggr_row['population'] = d3.sum(rows, function(d) { return d['population']; });
+                    return aggr_row;
+                })
+                .entries(new_data)
+
+            for (var i = 0; i < new_data.length; ++i) {
+                new_data[i] = new_data[i].values;
+            }
+        }
+        
+        drow_table(new_data)
+    }   
+
+    drow_table(data);
+    
 });
